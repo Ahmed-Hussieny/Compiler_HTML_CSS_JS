e44c6e9762dad0bbd84aaca6e55b2ed8e65069c5
Ahmed-Hussieny <ahmedalhoseny9991@icloud.com>
2023-08-10 22:00:09 +0300

    Updates

---
+++ precache-manifest.d1628b502f2e7ba396dae6bdcae20776.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae97ddb5dd34f6f833a95362b432de10",
+    "url": "/Compiler_HTML_CSS_JS/index.html"
+  },
+  {
+    "revision": "70c3ff1a8dd6beff5ca9",
+    "url": "/Compiler_HTML_CSS_JS/static/css/2.c46397a6.chunk.css"
+  },
+  {
+    "revision": "0abea8379b5cf03ee1cc",
+    "url": "/Compiler_HTML_CSS_JS/static/css/main.7d2118c4.chunk.css"
+  },
+  {
+    "revision": "70c3ff1a8dd6beff5ca9",
+    "url": "/Compiler_HTML_CSS_JS/static/js/2.4254f81d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Compiler_HTML_CSS_JS/static/js/2.4254f81d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0abea8379b5cf03ee1cc",
+    "url": "/Compiler_HTML_CSS_JS/static/js/main.81312cc9.chunk.js"
+  },
+  {
+    "revision": "fa8775cfde40e96303f7",
+    "url": "/Compiler_HTML_CSS_JS/static/js/runtime-main.feddf440.js"
+  }
+]);
